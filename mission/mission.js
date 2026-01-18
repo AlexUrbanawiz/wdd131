d39b3164a6557a4de3598c7e458b182464804f75
AlexUrbanawiz <koalapokemonfun@gmail.com>
2026-01-17 22:55:26 -0700

    Dark Mode for mission

--- mission/mission.js
+++ mission/mission.js
@@ -1,9 +1,21 @@
 let selectElem = document.getElementById('themelist');
-selectElem.addEventListener('change', function(){
+let bodyElem = document.querySelector('body');
+let imgElem = document.querySelector('img');
+selectElem.addEventListener('change', changeTheme);
+    
+function changeTheme() {
     let codeValue = selectElem.value;
     
     if(codeValue == 'Dark')
     {
-        document.querySelector('body').style.backgroundColor = 'darkgrey';
+        bodyElem.style.backgroundColor = '#333333';
+        bodyElem.style.color = '#fff';
+        imgElem.setAttribute('src', 'byui-logo-white.png');
     }
-})
+    else if(codeValue == "Light")
+    {
+        bodyElem.style.backgroundColor = "#fff"
+        bodyElem.style.color = '#000';
+        imgElem.setAttribute('src', 'byui-logo-blue.webp');
+    }
+}
