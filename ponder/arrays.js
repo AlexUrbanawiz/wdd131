0c1d5303cd70df68e7496c3366721ca849054bf9
AlexUrbanawiz <koalapokemonfun@gmail.com>
2026-02-09 21:34:03 -0700

    arrays

---
+++ ponder/arrays.js
@@ -0,0 +1,86 @@
+let pets = ["dog", "rhino", 'goldfish'];
+
+console.log(pets.length);
+
+pets[3] = "bunny"
+console.log(pets);
+pets.push("lizard");
+console.log(pets);
+
+const steps = ['one', 'two', 'three'];
+
+//.forEach call a function for every item in the array
+
+// steps.forEach(function(item){
+//     console.log(item);
+// })
+
+steps.forEach(showStep);
+
+function showStep(item) {
+    console.log(item);
+}
+
+
+//.map - also calls a function but creates a new array from the original array
+
+let myList = document.querySelector('#myList');
+
+const stepsHtml = steps.map(listTemplate);
+
+function listTemplate(item){
+    return `<li>${item}</li>`
+}
+
+myList.innerHTML = stepsHtml.join("");
+
+
+let grades = ['A', 'B', 'D'];
+let points;
+let gpaPoints = grades.map(convert)
+
+function convert(grade){
+    switch (grade){
+        case 'A':
+            points = 4;
+            break;
+        case 'B':
+            points = 3;
+            break;
+        case 'C':
+            points = 2;
+            break;
+        case 'D':
+            points = 1;
+            break;
+        case 'F':
+            points = 0;
+            break;
+        default:
+            alert("Not a valid grade");
+            break;
+    }
+    return points
+}
+console.log(gpaPoints)
+
+// .reduce reduce down to a single value with an acucmulator
+
+let totalPoints = gpaPoints.reduce(getTotal);
+function getTotal(total, item){
+    return total + item
+}
+
+console.log(totalPoints);
+
+let gpaAverage = totalPoints/gpaPoints.length;
+console.log(gpaAverage);
+
+// .filter makes a new array with only the items that pass
+const words = ['watermelon', 'peach', 'apple', 'tomato', 'grape'];
+
+
+const shortWords = words.filter(function(word){
+    return word.length < 6
+})
+console.log(shortWords)
